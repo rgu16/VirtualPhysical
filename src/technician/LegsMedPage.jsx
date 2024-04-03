0c8689910cee2783c6f5884f293212ff16d51a3c
Freewifii <michellezh2002@gmail.com>
2024-04-02 22:04:44 -0500

    add reference, upload image and summary to legs

--- src/technician/LegsMedPage.jsx
+++ src/technician/LegsMedPage.jsx
@@ -8,7 +8,7 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import axios from 'axios';
@@ -19,8 +19,16 @@ const LegsMedPage = (props) => {
  const [isHoveredTwo, setIsHoveredTwo] = useState(false);
  const [rightcalve, setRightCalfValue] = useState();
  const [leftcalve, setLeftCalfValue] = useState();
+ const [note, setNotes] = useState();
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const fileInputRef = useRef(null);
+    const [profilePic, setProfilePic] = useState()
 
+ const [isChecked, setIsChecked] = useState(false);
 
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
  const handleRightCalfChange = (event) => {
    setRightCalfValue(event.target.value)
  }
@@ -30,6 +38,47 @@ const LegsMedPage = (props) => {
    setLeftCalfValue(event.target.value)
  }
 
+ const handleUploadClick = () => {
+  fileInputRef.current.click();
+};
+
+const handleImageUpload = (e) => {
+  e.preventDefault();
+  const file = e.target.files[0];
+  console.log('UploadEyesImage')
+  setProfilePic(URL.createObjectURL(file))
+  if (!file) {
+      console.error('No file selected.');
+      return;
+  }
+  const formData = new FormData();
+  formData.append('file', file, file.name);
+  formData.append('location', "/eyes/Image")
+  console.log(formData)
+  axios({
+      method: "POST",
+      url: props.proxy+"/upload_file",
+      data: formData,
+      headers: {
+          Authorization: 'Bearer ' + props.token
+      }
+  }).then((response) => {
+    const res = response.data
+    console.log(res)
+ 
+    console.log('Server response:', response);
+    console.log('Image uploaded:', imageUrl);
+   // Assuming the URL is nested within a 'data' property, modify this accordingly
+  const imageUrl = response.data && response.data.url;
+   
+  }).catch((error)=>{
+      if(error.response){
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+      }
+  })
+};
 
  const handleSave = (e) => {
    e.preventDefault();
@@ -56,7 +105,7 @@ const LegsMedPage = (props) => {
     }
   })
  };
-
+{/*test */}
 
  return (
    <>
@@ -70,10 +119,9 @@ const LegsMedPage = (props) => {
          <div className="flex flex-col items-start justify-start max-w-[1700px] mx-auto md:px-5 w-full">
          <TabNav tab="legs"></TabNav>
            <div className="bg-white-A700 flex flex-col font-cairo items-center justify-start p-10 sm:px-5 w-full" >
-                      
+           
                   <div style={{paddingLeft: '150px', paddingTop: '50px'}} className="flex w-full min-h-screen p-5">
      <div className="w-full max-w-md">
-    
     
      <div className="absolute bg-white-A700 bottom-[8%] flex flex-col font-cairo gap-6 h-[1000px] md:h-auto inset-x-[0] justify-start max-w-[1695px] mx-auto pb-6 pt-8 px-5 rounded-bl-[12px] rounded-br-[12px] w-full">
 
@@ -99,19 +147,9 @@ const LegsMedPage = (props) => {
 
                     </div>
             
-                    <Img onMouseEnter={() => setIsHoveredOne(true)}
-                     onMouseLeave={() => setIsHoveredOne(false)}
-                      className="h-[43px] w-[43px]"
-                      src="images/img_profile_black_900.svg"
-                      alt="profile"
-                    />
+                   
                   </div>
-                  <Img onMouseEnter={() => setIsHoveredTwo(true)}
-                   onMouseLeave={() => setIsHoveredTwo(false)}
-                    className="h-[43px] w-[43px]"
-                    src="images/img_profile_black_900.svg"
-                    alt="profile_One"
-                  />
+                 
                 </div>
                 <div className="flex flex-col md:gap-10 gap-[301px] justify-start">
                   <Text
@@ -131,6 +169,55 @@ const LegsMedPage = (props) => {
                 </div>
               </div>
             </div>
+            
+            <div className="absolute top-30 right-20 w-1/2" style={{paddingLeft: '300px'}}>
+ 
+ <div className= "flex flex-col items-start justify-start w-[400px] h-full m-[50px] mt-[80px]">
+        <Text className="font-bold text-2xl text-black-900">Notes: </Text>
+        <textarea className="w-full h-[200px] border border-gray-400 border-2 rounded-[14px] p-[10px]" 
+                  placeholder="Medical Technician notes"
+                  value={note}
+                  onChange={(e) => setNotes(e.target.value)}></textarea>
+       
+        {/*Upload Image */}
+        <div style={{ paddingTop: "2rem" }}>
+            <input 
+                    ref={fileInputRef}
+                    type="file"
+                    style={{ display: 'none'}}
+                    accept="image/*" // Accept only image files
+                    onChange={handleImageUpload}
+                  />
+                  <button
+  className="flex md:flex-col flex-row md:gap-5 items-center mt-2.5 w-[96%] md:w-full border-0 roundedButton"
+  style={{ background: '#5974F6',  borderRadius: '20px', width: '250px'}}
+  onClick={handleUploadClick}
+>
+  <Img
+    className="h-7 md:ml-[0] ml-[0] md:mt-0 mt-1 w-7 "
+    src="images/audioupload.png"
+    alt="television"
+  />
+  <Text  style={{color: 'white' }} className="font-semibold ml-2.5 md:ml-[0] text-xl">Upload Image of Eyes</Text>
+</button>
+                  <Img
+                      className="h-[130px] md:h-auto rounded-[50%] w-[130px] md:h-auto object-cover  w-full"
+                      src= {profilePic}
+                      alt=""
+                      onLoad ={()=> setImageLoaded(true)}
+                      // style = {{display: imageLoaded? "none": "block"}}
+                      />
+                      
+   
+
+    </div>
+
+        </div>
+               
+               </div>   
+               
+               
+            {/* 
             <Img style = {{ opacity: isHoveredOne ? 1 : 0, // Show the image if hovered, otherwise hide
              transition: 'opacity 0.3s ease', }} // Add a smooth transition effect
               className="absolute h-[270px] object-cover right-[3%] top-[6%] w-[25%]"
@@ -142,7 +229,7 @@ const LegsMedPage = (props) => {
               className="absolute bottom-[10%] h-[465px] object-cover left-[73.5%] w-[31%]"
               src="images/edema.png"
               alt="screenshot20231_One"
-            />
+            />*/}
             {/*  <div className="absolute bottom-[0] flex flex-col items-center justify-start left-[0] w-[47%]">*/}
              {/*   <div className="flex flex-col items-center justify-start w-full">*/}
                  {/* <div className="flex flex-col items-center justify-end w-full">*/}
@@ -163,6 +250,7 @@ const LegsMedPage = (props) => {
            Check for pitting edema and classify severity for each region: {" "}
           
         </h4>
+        <FormLabel style={{ paddingBottom: '25px', paddingTop: '5px', fontSize: '17px', color: 'black' }} id="demo-row-radio-buttons-group-label">Hint: Check the severity of swelling in the calves, caused by excess fluid, by applying pressure to the swollen area</FormLabel>
         {/*i. Right calve */}
         <FormControl value = {rightcalve}
      onChange={handleRightCalfChange}>
@@ -200,6 +288,37 @@ const LegsMedPage = (props) => {
  <FormLabel style={{paddingTop: '9px', fontSize: '20px' }} id="demo-row-radio-buttons-group-label">Severe</FormLabel>
      </RadioGroup>
    </FormControl>
+
+   <div>
+   {isChecked && (
+        <div style={{ marginLeft: '50px' }}>
+          {/* Your images */}
+          <img
+            style={{
+              width: "350px", // Enlarge the width of the image
+              height: "220px", // Set height to auto to maintain aspect ratio
+              paddingTop: "5px",
+              marginRight: '15px'
+
+            }}
+            src="images/pittingedema.png"
+            alt="screenshot20231"
+          />
+         
+         
+        </div>
+      )}
+       <label>
+        <input
+          type="checkbox"
+          className="cboxes"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        Show Reference Images
+      </label>
+    </div>
+  
     <div style={{paddingTop: "2rem"}}>
      <Stack spacing={2} direction="row">
     {/*  <Link to="/eyes"> <Button variant="text">Previous Section</Button></Link>*/}
